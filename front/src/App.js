79e5a50f404e711bc43a247ee629a2267e79d96a
Tuuli Silvennoinen <tuuli.silvennoinen@tuni.fi>
2023-02-05 18:36:37 +0200

    create app and some design of elemnts

---
+++ front/src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <div class="header">
+        <h1>Art Portfolio</h1>
+        <ul>
+          <li id="li">Traditional</li>
+          <li id="li">Digital</li>
+          <li id="li">Video</li>
+        </ul>
+      </div>
+      <div class="img">
+        <li id="li2">img1</li>
+        <li id="li2">img2</li>
+        <li id="li2">img3</li>
+      </div>
+    </div>
+  );
+};
+
+export default App;
